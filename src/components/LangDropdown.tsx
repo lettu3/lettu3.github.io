dd46d020ce3c0612d600a8892941011047a38338
Ignacio <ignas.gomezb12@gmail.com>
2025-01-31 06:20:41 -0300

    Lang Dropdown menu

---
+++ src/components/LangDropdown.tsx
@@ -0,0 +1,32 @@
+import Dropdown from './Dropdown.tsx';
+import {Item} from './Dropdown.tsx';
+import i18next from 'i18next';
+
+type LangDropdownProps = {
+    items: Item[];
+};
+
+function LangDropDown ({items} : LangDropdownProps) {
+    const onItemSelect = (item: Item) => {
+        switch (item.title) {
+            case "[EN] - English":
+                i18next.changeLanguage('en');
+                break;
+            case "[ES] - Espanol":
+                i18next.changeLanguage('es');
+                break;
+            case "[JP] - 日本語":
+                i18next.changeLanguage('jp');
+                break;
+            default:
+                i18next.changeLanguage('en');
+                break;
+        }
+    };
+    return (
+        <Dropdown items={items} onItemSelect={onItemSelect}/>
+    );
+}
+
+
+export default LangDropDown;
