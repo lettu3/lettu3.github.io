e5a3213f1ac54b6ae865adcec5ccc4b23e107d61
Ignacio <ignas.gomezb12@gmail.com>
2025-02-11 14:00:57 -0300

    not necessary

--- src/containers/BodyProjects/BodyProjects.tsx
+++ src/containers/BodyProjects/BodyProjects.tsx
@@ -3,13 +3,6 @@ import ProjectList from '../ProjectList/ProjectList';
 
 
 function BodyProjects () {
-    const ProjectEntry0 = {
-        title: 'EXAMPLE',
-        date: 'February 2, 2025',
-        description: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        link: '/',
-        image: '/images/dasd.webp',      
-    };
     return (
         <div className="BodyProjects">
             <ProjectList />
