07370e1ca43756e1ad98f7254a9f3bb18a37e021
Ignacio <ignas.gomezb12@gmail.com>
2025-02-18 20:38:55 -0300

    about newpage, responsive, adjust it later

--- src/containers/About/About.tsx
+++ src/containers/About/About.tsx
@@ -1,23 +1,19 @@
 /* About.tsx */
+import './About.css';
 import { useTranslation } from "react-i18next";
 import Personal from "../../components/Personal";
-import './About.css';
 
 function About () {
     const  {t} = useTranslation();
 
     return (
-        <div className="about">
-            <div className="about column-1">
-                <div className="about column-1 row-1">
-                    <h1>{t('SARASA')}</h1>
-                </div>
-                <div className="about column-1 row-2">
-                    <p>{t('lorem')}</p>
-                </div>
+        <div className="about-container">
+            <div className="about-header">
+                <h1>{t('SARASA')}</h1>
             </div>
-            <div className="about column-2">
-                <Personal />
+            <div className='about-content'>
+                <div className="about-pp"><Personal /></div>
+                <div className="about-body"><p>{t('lorem')}</p></div>
             </div>
         </div>
     );
