f31f6ec92b982147275eff043e34bf92c26635a0
Ignacio <ignas.gomezb12@gmail.com>
2025-02-19 10:39:32 -0300

    home icon new}

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,23 @@
 /* Home.tsx */
+import { RootState } from '../state/store';
 import './Home.css';
 import { useLocation, useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
 
 function Home () {
+    const theme = useSelector((state: RootState) => state.theme.value);
     const route = useLocation().pathname;
     const navigate = useNavigate();
+
+    const [currentIcon, setCurrentIcon] = useState<string>(
+        theme === 'light' ? '/homeLight.svg' : '/homeDark.svg'
+    );
+
+    useEffect(() => {
+        const icon = theme === 'light' ? '/homeLight.svg' : '/homeDark.svg';
+        setCurrentIcon(icon);
+    }, [theme]);
 
     const handleClick = () => {
         if (route !== '/') {
@@ -16,7 +29,7 @@ function Home () {
         <button 
         className='home'
         onClick={handleClick}>
-            本
+            <img src={currentIcon} className='home-icon' />
         </button>
     );
 }
