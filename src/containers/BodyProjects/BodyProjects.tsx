2b5598540416aa660811dbad2e53bea886f59e70
Ignacio <ignas.gomezb12@gmail.com>
2025-02-19 21:31:04 -0300

    header translation

--- src/containers/BodyProjects/BodyProjects.tsx
+++ src/containers/BodyProjects/BodyProjects.tsx
@@ -1,11 +1,13 @@
 import './BodyProjects.css';
 import ProjectList from '../ProjectList/ProjectList';
+import { useTranslation } from 'react-i18next';
 
 
 function BodyProjects () {
+    const {t} = useTranslation();
     return (
         <div className="BodyProjects">
-            <h1>PROJECTS</h1>
+            <h1>{t('projects').toUpperCase()}</h1>
             <ProjectList />
         </div>
     );
