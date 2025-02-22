98adf7a15cf5e18125cd32482fcb4b2fd58fe8eb
Ignacio <ignas.gomezb12@gmail.com>
2025-02-22 20:12:03 -0300

    colors

--- src/components/TechCard/TechCard.tsx
+++ src/components/TechCard/TechCard.tsx
@@ -1,14 +1,17 @@
 import './TechCard.css';
+import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../state/store';
 
 type TechCardProps = {
     title: string;
+    hoverColor: string;
 };
 
 
-function TechCard ({title}: TechCardProps) {
+function TechCard ({title, hoverColor}: TechCardProps) {
     const theme = useSelector((state: RootState) => state.theme.value);
+    const [isHovered, setIsHovered] = useState(false);
 
     const imgName = theme === 'light' ? `${title.toLowerCase()}Light.svg` : `${title.toLowerCase()}Dark.svg`;
 
@@ -36,13 +39,21 @@ function TechCard ({title}: TechCardProps) {
             break;
     }
 
+    const handleMouse = () => {
+        setIsHovered(!isHovered);
+        console.log("mouse event");
+    };
+
     return (
-        <div className='tech-card'>
+        <div className='tech-card' 
+             onMouseEnter={handleMouse} onMouseLeave={handleMouse}
+             style={{ backgroundColor: isHovered ? `#${hoverColor}` : 'var(--footer-bg-color)' }}
+        >
             <div className='tech-card-logo'>
                 <img src={imgPath} />
             </div>
             <div className='tech-card-title'>
-                {titleN}
+                {titleN}{isHovered}
             </div>
         </div>
     );
--- src/containers/TechContainer/TechContainer.tsx
+++ src/containers/TechContainer/TechContainer.tsx
@@ -3,15 +3,31 @@ import './TechContainer.css';
 import TechCard from "../../components/TechCard/TechCard";
 
 function TechContainer(){
-    const technologies = ["TypeScript", "JavaScript", "Python", "Java",
-                          "Clang", "Cpp", "Haskell", "arm", "React",
-                          "Redux", "FastAPI", "ApacheSpark", "MySQL",
-                          "MongoDB", "Git", "HTML", "Docker", "Linux",
-                          "Bash"];
+    const technologies = [
+        {name: "TypeScript", color: "3178c6"},
+        {name: "JavaScript", color: "f7df1e"},
+        {name: "Python", color: "4584b6"},
+        {name: "Java", color: "f8981d"},
+        {name: "Clang", color: "00599c"},
+        {name: "Cpp", color: "00599c"},
+        {name: "Haskell", color: "5e5086"},
+        {name: "arm", color: "0091bd"},
+        {name: "React", color: "61dafb"},
+        {name: "Redux", color: "764abc"},
+        {name: "FastAPI", color: "009688"},
+        {name: "ApacheSpark", color: "e25a1c"},
+        {name: "MySQL", color: "f8981d"},
+        {name: "MongoDB", color: "00ed64"},
+        {name: "Git", color: "f03c2e"},
+        {name: "HTML", color: "e44d26"},
+        {name: "Docker", color: "066da5"},
+        {name: "Linux", color: "f6da4a"},
+        {name: "Bash", color: "4da825"}
+    ]
     return (
         <div className='tech-container'>
             {technologies.map((tech, index) => (
-                <TechCard key={index} title={tech} />
+                <TechCard key={index} title={tech.name} hoverColor={tech.color} />
             ))}
         </div>
     )
