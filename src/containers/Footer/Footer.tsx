462def51acd2bdb134ffa24c03a5e2f8ab58fbb1
Ignacio <ignas.gomezb12@gmail.com>
2025-02-23 11:36:40 -0300

    footer icons

--- src/containers/Footer/Footer.tsx
+++ src/containers/Footer/Footer.tsx
@@ -1,9 +1,16 @@
 /* Footer.tsx */
 import { useTranslation } from 'react-i18next'
 import './Footer.css';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../state/store';  
 
 export default function Footer () {
+    const theme = useSelector((state: RootState) => state.theme.value);
     const {t} = useTranslation();
+
+    const githubIcon = theme === 'light' ? './githubLight.svg' : './githubDark.svg';
+    const linkedinIcon = theme === 'light' ? './linkedinLight.svg' : './linkedinDark.svg';
+
     return (
         <div className='footer-component'>
             <div className='footer-columns'>
@@ -13,8 +20,18 @@ export default function Footer () {
                 </div>
                 <div className='footer-social'>
                     <h1>{t('social')}</h1>
-                    <p><a href="https://github.com/lettu3" target="_blank" rel="noopener noreferrer">Github</a></p>
-                    <p><a href="https://linkedin.com/in/ignacio-gomez-barrios-53701a172/" target="_blank" rel="noopener noreferrer">LinkedIn</a></p>
+                    <p>
+                        <a href="https://github.com/lettu3" target="_blank" rel="noopener noreferrer" className='social-link'>
+                            <img src={githubIcon} alt="github-icon" />
+                            Github
+                        </a>
+                    </p>
+                    <p>
+                        <a href="https://linkedin.com/in/ignacio-gomez-barrios-53701a172/" target="_blank" rel="noopener noreferrer" className='social-link'>
+                            <img src={linkedinIcon} alt="linkedin-icon" />
+                            LinkedIn
+                        </a>
+                    </p>
                 </div>
             </div>
             <div className='footer-bottom'>
