a3d2234bc6268214029d7519f5781378ac64b14b
Ignacio <ignas.gomezb12@gmail.com>
2025-02-25 19:28:26 -0300

    ident pretty

--- src/containers/Body/Body.tsx
+++ src/containers/Body/Body.tsx
@@ -8,21 +8,21 @@ export default function Body () {
 
     return (
         <div className="body-component">
-        <div className="body-column-1">
-            <div className="welcome">
-                <h1>
-                {t('welcome')}
-                </h1>
-            </div>
-            <div className="index">
-                <h1>{t('index').toUpperCase()}</h1>
-                <div>
-                    <p>/<Link to="/about">{t('about')}</Link></p>
-                    <p>/<Link to="/projects">{t('projects')}</Link></p>
-                    <p>/<a href="https://lettu3.github.io/offtopic">{t('offtopic')}</a></p>
+            <div className="body-column-1">
+                <div className="welcome">
+                    <h1>
+                    {t('welcome')}
+                    </h1>
+                </div>
+                <div className="index">
+                    <h1>{t('index').toUpperCase()}</h1>
+                    <div>
+                        <p>/<Link to="/about">{t('about')}</Link></p>
+                        <p>/<Link to="/projects">{t('projects')}</Link></p>
+                        <p>/<a href="https://lettu3.github.io/offtopic">{t('offtopic')}</a></p>
+                    </div>
                 </div>
             </div>
-        </div>
         </div>
     );
 }
