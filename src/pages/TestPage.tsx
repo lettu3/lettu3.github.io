1b3bc040020961c96f9fb1c07daaa06967052f87
Ignacio <ignas.gomezb12@gmail.com>
2025-02-26 18:45:50 -0300

    testing

--- src/pages/TestPage.tsx
+++ src/pages/TestPage.tsx
@@ -3,7 +3,8 @@ import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../state/store";
 
-import TechContainer from '../containers/TechContainer/TechContainer';
+import Theme from '../components/Theme';
+import Card from '../components/Card/Card';
 
 function TestPage () {
     //apply theme
@@ -14,7 +15,10 @@ function TestPage () {
 
     return (
         <div className="TestPage">
-            <TechContainer/>
+            <Theme/>
+            <Card header='hola' body= 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'> 
+                <button>holas</button>
+            </Card>
         </div>
     );
 }
