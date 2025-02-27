99e00640287d3bc2138ca36e81e5f0775fc7ecdd
Ignacio <ignas.gomezb12@gmail.com>
2025-02-27 12:35:29 -0300

    FIX: now contact modal correctly fits all the screen

--- src/components/ContactModal/ContactModal.tsx
+++ src/components/ContactModal/ContactModal.tsx
@@ -1,5 +1,6 @@
 import './ContactModal.css';
 import { useTranslation } from "react-i18next";
+import  ReactDOM  from 'react-dom';
 
 type ContactModalProps = {
     onClose: () => void;
@@ -8,7 +9,7 @@ type ContactModalProps = {
 function ContactModal({onClose}: ContactModalProps) {
     const {t} = useTranslation();
 
-    return(
+    return ReactDOM.createPortal(
         <div className="contact-modal-overlay">
             <div className="contact-modal-content">
                 <h1>{t('contact')}</h1>
@@ -16,7 +17,8 @@ function ContactModal({onClose}: ContactModalProps) {
                 <p>ignas.gomezb12@gmail.com</p>
                 <button className="contact-close-button" onClick={onClose}>{t("close")}</button>
             </div> 
-        </div>
+        </div>,
+        document.getElementById("modal-root") as HTMLElement
     );
 };
 
