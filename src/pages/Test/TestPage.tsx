bc11bd48dd994d95026d9a68cfacef76e0439641
Ignacio <ignas.gomezb12@gmail.com>
2025-02-28 08:21:40 -0300

    REFACTOR: testing

--- src/pages/Test/TestPage.tsx
+++ src/pages/Test/TestPage.tsx
@@ -1,23 +1,19 @@
 import './TestPage.css'
 import { useEffect } from "react";
-import { useSelector } from "react-redux";
-import { RootState } from "../state/store";
 
-import Theme from '../../components/Theme';
-import Card from '../../components/Card/Card';
-
+                                                                               
 function TestPage () {
 
     useEffect(() => {
         document.body.classList.add("testing-mode");
 
         return () => {
-            document.body.classList.remove("testing-mode");
-        };
+            document.body.classList.remove("testing-mode");                   
+        }; 
     }, []);
 
     return (
-        <div className="TestPage">
+        <div className="test-page">
             <h1>Testing Page</h1>
         </div>
     );
