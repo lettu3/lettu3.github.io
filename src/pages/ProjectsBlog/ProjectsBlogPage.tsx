49d1f5e9fb5330f838f96ed7a112b5d7815f0203
Ignacio <ignas.gomezb12@gmail.com>
2025-02-28 11:42:48 -0300

    markdown guides for code

--- src/pages/ProjectsBlog/ProjectsBlogPage.tsx
+++ src/pages/ProjectsBlog/ProjectsBlogPage.tsx
@@ -6,26 +6,27 @@ import { useState, useEffect } from 'react';
 import Markdown from 'react-markdown';
 import CodeBlock from '../../components/CodeBlock/CodeBlock';
 
+
 function ProjectsBlogPage() {
     const params = useParams();
-    const {i18n} = useTranslation();
+    const { i18n } = useTranslation();
     const currentLang = i18n.language;
 
     const blog = projectsBlogs
-                 .find(blog => (blog.title) === (params.title + "-title"));
+        .find(blog => (blog.title) === (params.title + "-title"));
     const [content, setContent] = useState<string>("");
 
     useEffect(() => {
         if (blog) {
             const langContent = blog.content[currentLang as keyof typeof blog.content]
-                                || blog.content.en; //fallback to english
+                || blog.content.en; //fallback to english
             const loadMarkdown = async () => {
-                try{
+                try {
                     const response = await fetch(langContent);
                     const text = await response.text();
                     setContent(text);
                 }
-                catch{
+                catch {
                     console.error("Error loading markdown");
                     setContent("Error loading markdown");
                 }
@@ -36,19 +37,24 @@ function ProjectsBlogPage() {
     }, [blog]);
 
     return (
-      <div className="projects-blog-page">
-        <Markdown
-            children={content}
-            components={{
-                code({node, inline, className, children, ...props}){
-                    if (!inline){
-                        return <CodeBlock code={String(children).replace(/\n$/, '')} />;
+        <div className="projects-blog-page">
+            <Markdown
+                children={content}
+                components={{
+                    code(props) {
+                        const { children, className, node, ...rest } = props
+                        const match = /language-(\w+)/.exec(className || '')
+                        return match ? (
+                            <CodeBlock code={String(children).replace(/\n$/, '')}/>
+                        ) : (
+                            <code {...rest} className={className}>
+                                {children}
+                            </code>
+                        )
                     }
-                    return <code {...props}>{children}</code>;
-                }
-            }}
-        />
-      </div>  
+                }}
+            />
+        </div>
     );
 
 }
